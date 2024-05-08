11722a34ca2edf46c4c095cb6d23756444a3cf74
ottargudna <ottargudna@gmail.com>
2024-05-08 15:16:12 +0000

    manufacturers app

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobhunter/', include('jobhunter.urls'))
+    path('jobhunter/', include('jobhunter.urls')),
+    path('manufacturer/', include('manufacturer.urls'))
 ]
